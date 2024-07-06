78fc75d160a1d1d2ed06ddcdd611ad399fcd3ac5
CoderOJ <CoderOJ@outlook.com>
2024-07-06 13:19:43 +0800

    upd(src/main): update `read_candidate`
      use closure instead of passing candidate list

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		Box::new(Cmd::new())
 	};
 
-	let read_candidate = |list: &Vec<Word>| -> Result<Word, Error> {
-		let word = inter.read_word()?;
-		return if list.iter().any(|s| word_eq(&word, s)) {
-			Ok(word)
-		} else {
-			Err(Error::Unkown)
-		};
-	};
-	let list_final: Vec<Word> = builtin_words::FINAL
-		.iter()
-		.map(|&s| word_from_str(s).unwrap())
-		.collect();
-	let list_acceptalbe: Vec<Word> = builtin_words::ACCEPTABLE
-		.iter()
-		.map(|&s| word_from_str(s).unwrap())
-		.collect();
+	// `inter` must be passed because
+	// 1. functions can not capture it
+	// 2. specific lifetime for closures is still experimental
+	fn get_reader<'a>(
+		list: &'a Vec<Word>,
+		inter: &'a Box<dyn Interactor>,
+	) -> impl 'a + FnMut() -> Result<Word, Error> {
+		|| {
+			let word = inter.read_word()?;
+			return if list.iter().any(|s| word_eq(&word, s)) {
+				Ok(word)
+			} else {
+				Err(Error::Unkown)
+			};
+		}
+	}
 
-	let mut plate = Plate::new(&read_candidate(&list_final)?);
+	// rust does not automaticly extend lifetime like cpp does to a rebinded xvalue
+	// so it is required to explicitly bind it to `list_final`, which has a explicit lifetime
+	let parse_list = |list: &[&str]| list.iter().map(|&s| word_from_str(s).unwrap()).collect();
+	let list_final: Vec<Word> = parse_list(builtin_words::FINAL);
+	let list_acceptalbe: Vec<Word> = parse_list(builtin_words::ACCEPTABLE);
+	let mut read_final = get_reader(&list_final, &inter);
+	let mut read_acceptable = get_reader(&list_acceptalbe, &inter);
+
+	let mut plate = Plate::new(&read_final()?);
 	while !plate.is_win() && plate.count() < 6 {
 		let word = loop {
-			match read_candidate(&list_acceptalbe) {
+			match read_acceptable() {
 				Ok(word) => break word,
 				Err(_) => println!("INVALID"),
 			}
