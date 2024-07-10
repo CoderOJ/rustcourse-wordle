2340760694f13a4357b0e35b78b7fe2735e347d6
CoderOJ <CoderOJ@outlook.com>
2024-07-11 01:33:58 +0800

    feat(config): -c <CONFIG_FILE>

--- src/config.rs
+++ src/config.rs
@@ -2,41 +2,28 @@ use {
 	crate::{builtin_words, plate::*},
 	anyhow::{anyhow, Result},
 	clap::Parser,
+	serde::Deserialize,
+	serde_json::from_str,
 	std::{collections::HashSet, io::BufRead, str::from_utf8},
 };
 
-#[derive(Parser, Debug)]
+#[derive(Default, Deserialize, Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
 	/// select answer mode, conflict to -r
-	#[arg(short, long, conflicts_with = "random")]
+	#[arg(short, long)]
 	word: Option<String>,
 
 	/// random mode, conflict to -s
-	#[arg(short, long, default_value_t = false, conflicts_with = "word")]
+	#[arg(short, long, default_value_t = false)]
 	random: bool,
+
 	/// random seed, requires -r
-	#[arg(
-		short,
-		long,
-		default_value_t = 0,
-		value_name = "SEED",
-		requires = "random",
-		// redundent for: clap seems to treat `-r`` as *provided* when `-w` presents
-		conflicts_with = "word",
-	)]
-	seed:   u64,
+	#[arg(short, long)]
+	seed: Option<u64>,
 	/// random date, requires -r
-	#[arg(
-		short,
-		long = "day",
-		default_value_t = 1,
-		value_name = "DAY",
-		requires = "random",
-		// redundent for: clap seems to treat `-r`` as *provided* when `-w` presents
-		conflicts_with = "word",
-	)]
-	date:   u32,
+	#[arg(short, long, value_name = "DAY")]
+	day:  Option<u32>,
 
 	/// difficult mode
 	#[arg(short = 'D', long, default_value_t = false)]
@@ -48,6 +35,7 @@ struct Args {
 
 	/// final word set
 	#[arg(short = 'f', long = "final-set", value_name = "FINAL_SET_FILE")]
+	#[serde(rename = "final_set")] 
 	final_set_src: Option<String>,
 
 	/// acceptable word set
@@ -56,11 +44,18 @@ struct Args {
 		long = "acceptable-set",
 		value_name = "ACCEPTABLE_SET_FILE"
 	)]
+	#[serde(rename = "acceptable_set")] 
 	acceptable_set_src: Option<String>,
 
 	/// state json file
 	#[arg(short = 'S', long = "state", value_name = "STATE_FILE")]
+	#[serde(rename = "state")] 
 	state_src: Option<String>,
+
+	/// default config file
+	#[arg(short, long = "config", value_name = "CONFIG_FILE")]
+	#[serde(rename = "config")] 
+	config_src: Option<String>,
 }
 
 pub enum WordSrc {
@@ -83,7 +78,23 @@ pub struct Config {
 }
 
 pub fn config() -> Result<Config> {
-	let args = Args::parse();
+	let args0 = Args::parse();
+	let args1: Args = match args0.config_src {
+		None => Default::default(),
+		Some(path) => from_str(&std::fs::read_to_string(&path)?)?,
+	};
+	let args = Args {
+		word:               args0.word.or(args1.word),
+		random:             args0.random || args1.random,
+		seed:               args0.seed.or(args1.seed),
+		day:                args0.day.or(args1.day),
+		difficult:          args0.difficult || args1.difficult,
+		stats:              args0.stats || args1.stats,
+		final_set_src:      args0.final_set_src.or(args1.final_set_src),
+		acceptable_set_src: args0.acceptable_set_src.or(args1.acceptable_set_src),
+		state_src:          args0.state_src.or(args1.state_src),
+		config_src:         None,
+	};
 
 	let parse_builtin_list =
 		|list: &[&str]| list.iter().map(|&s| word_from_str(s).unwrap()).collect();
@@ -106,11 +117,11 @@ pub fn config() -> Result<Config> {
 		return Err(anyhow!("list_fianl is not subset of list_acceptable"));
 	}
 
-	let word_src: WordSrc = match (args.word, args.random, args.seed, args.date) {
+	let word_src: WordSrc = match (args.word, args.random, args.seed, args.day) {
 		(None, false, _, _) => WordSrc::Ask,
-		(None, true, seed, date) => WordSrc::Random(seed, date),
-		(Some(word_str), false, _, _) => WordSrc::Select(word_from_str(word_str.as_str())?),
-		_ => unreachable!(),
+		(None, true, seed, date) => WordSrc::Random(seed.unwrap_or(0), date.unwrap_or(1)),
+		(Some(word_str), false, None, None) => WordSrc::Select(word_from_str(word_str.as_str())?),
+		_ => Err(anyhow!("arguments conflict!"))?,
 	};
 
 	return Ok(Config {
